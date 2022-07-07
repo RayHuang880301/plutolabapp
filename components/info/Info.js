c21aadf8990689a7f73c4d99df5fff454fff75e0
abner.huang <abner.huang@tkspring.com>
2022-07-07 23:21:46 +0800

    update

--- components/info/Info.js
+++ components/info/Info.js
@@ -5,15 +5,23 @@ import styles from '../info/Info.module.css'
 import Image from 'next/image'
 import Window from '../window/Window'
 import WindowLag from '../windowLag/WindowLag'
+import ReactDOM from 'react-dom';
+import Draggable from 'react-draggable';
 
 const Info = () => {
   return (
     <div className={styles.section}>
-      <div className={styles.container}>
-        <WindowLag top={10} left={5} width={400} height={185}>Welcome to Pluto Mateverse</WindowLag>
-        <div className={styles.logo} data-aos="zoom-in" data-aos-anchor-placement="center-bottom" data-aos-duration="1000">
-          <Image src={plutoLabLogo} width={400} height={270} alt=''/>
+      <Draggable>
+        <div>
+          <div className="handle">Drag from here</div>
+          <div>This readme is really dragging on...</div>
         </div>
+      </Draggable>
+        {/* <WindowLag top={10} left={5} width={400} height={185}>Welcome to Pluto Mateverse</WindowLag> */}
+      <div className={styles.container}>
+        {/* <div className={styles.logo} data-aos="zoom-in" data-aos-anchor-placement="center-bottom" data-aos-duration="1000">
+          <Image src={plutoLabLogo} width={400} height={270} alt=''/>
+        </div> */}
         <div className={styles.bottomCard} data-aos="fade-down" data-aos-easing="linear" data-aos-anchor-placement="top-bottom" data-aos-duration="1000">
           <h2>Previous Work</h2>
           <div className={styles.arrow}><Image src={downArrow} width={30} height={30} alt=''/></div>
--- components/windowLag/WindowLag.js
+++ components/windowLag/WindowLag.js
@@ -5,9 +5,14 @@ import symbol from '../../assets/symbol.svg'
 import substract from '../../assets/substract.svg'
 import reactangle from '../../assets/rectangle.svg'
 import close from '../../assets/close.svg'
-
+import ReactDOM from 'react-dom';
+import Draggable from 'react-draggable';
 const WindowLag = (props) => {
   const { children, top, left, width, height, content} = props
+
+  // TODO: drag and drop
+  // TODO: move and leave shadow
+  // TODO: (optional) HTML animation frame for performance
 
   const [windowStyle, setWindowStyle] = useState({
     top: `${top}%`,
@@ -23,22 +28,42 @@ const WindowLag = (props) => {
     height: `${height}px`
   })
 
+  const handle = (e) => {
+    console.log(e)
+  }
+  const nodeRef = React.useRef(null);
+
+
   return (
-    <>
-      <div className={styles.cardShadow} style={cardStyle}>
-        <div className={styles.boxShadow} />
-      </div>
-      <div className={styles.window} style={windowStyle}>
-        <div className={styles.topBox}>
-          <div className={styles.img}><Image src={substract} width={14} height={14} alt='' /></div>
-          <div className={styles.img}><Image src={reactangle} width={14} height={14} alt='' /></div>
-          <div className={styles.img}><Image src={close} width={14} height={14} alt='' /></div>
+    <Draggable
+    nodeRef={nodeRef}
+    handle=".handle"
+        defaultPosition={{x: 0, y: 0}}
+        position={null}
+        grid={[25, 25]}
+        scale={1}
+        onStart={handle}
+        onDrag={handle}
+        onStop={handle}
+         >
+      <div ref={nodeRef}>
+        {/* <div className={styles.cardShadow} style={cardStyle}>
+          <div className={styles.boxShadow} />
+        </div> */}
+        <div className={styles.window} style={windowStyle}>
+          <div className={styles.topBox}>
+            <h1>TEST</h1>
+            {/* <div className={styles.img}><Image src={substract} width={14} height={14} alt='' /></div>
+            <div className={styles.img}><Image src={reactangle} width={14} height={14} alt='' /></div>
+            <div className={styles.img}><Image src={close} width={14} height={14} alt='' /></div> */}
+          </div>
+          <div className={styles.content}>
+            {children}
+          </div>
         </div>
-        <div className={styles.content}>
-          {children}
-        </div>
+
       </div>
-    </>
+    </Draggable>
   )
 }
 
